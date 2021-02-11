dbf780ee9e5dccd5449e80cdf9935768630ec2f4
msr1013 <rawalsinghmanish@gmail.com>
2021-02-11 16:39:45 +0545

    'chat-template'

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.view1}></View>
+      <View style={styles.view1}>
+        <Text style={styles.text}>Conversation</Text>
+      </View>
       <View style={styles.view2}></View>
       <View style={styles.view3}></View>
     </View>
@@ -15,26 +17,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end'
+    flexDirection: 'column',
+
 
   },
   view1: {
-    width: 100,
-    height: 100,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#f00'
   },
   view2: {
-    width: 100,
-    height: 100,
+    flex: 8,
     backgroundColor: '#ff0'
   },
   view3: {
-    width: 100,
-    height: 100,
+    flex: 1,
     backgroundColor: '#00f'
   },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'fff'
+  }
 })
 
 export default App
