413a37ff737e110132c579bc62f72724003aad76
msr1013 <rawalsinghmanish@gmail.com>
2021-02-14 14:53:01 +0545

    'custom-button'

--- widgets/HomePage.js
+++ widgets/HomePage.js
@@ -13,6 +13,8 @@ const AppBar = props => {
 }
 
 const TypeCard = props => {
+
+
     return (
         <View style={{ ...styles.typeCard, backgroundColor: props.color }}>
             <Ionicons name={props.icon} size={50} color={Colors.blue} />
@@ -20,6 +22,12 @@ const TypeCard = props => {
             <Text style={styles.title}>{props.title}</Text>
         </View>
     )
+}
+
+const CustomButton = props => {
+    return <View style={{ ...styles.button, backgroundColor: props.bgc }}>
+        <Text style={{ color: props.color }}>{props.title}</Text>
+    </View>
 }
 
 const HomePage = props => {
@@ -31,9 +39,16 @@ const HomePage = props => {
                 <Text style={styles.balanceHeading}>Your Balance</Text>
                 <Text style={styles.balance}>$547000.00</Text>
                 <View style={styles.typeCards}>
-
+                    <TypeCard color={Colors.lightBlue} icon='md-notifications-outline' title='Expenses' amount='$5000' />
                 </View>
-
+            </View>
+            <View style={styles.transactionContainer}>
+                <View style={{ ...styles.container, marginTop: 15 }}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.amount}>Transactions</Text>
+                        <CustomButton title='View All' color={Colors.blue} bgc='#abc'> </CustomButton>
+                    </View>
+                </View>
             </View>
         </View>
     )
@@ -44,6 +59,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: Colors.blue
     },
+    container: {
+        paddingHorizontal: 40,
+        marginVertical: 40
+
+    },
     appbar: {
         flexDirection: 'row',
         justifyContent: 'space-between'
@@ -52,6 +72,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop: 30
+    },
+    typeCard: {
+        width: '48%',
+        height: 180,
+        borderRadius: 10,
+        padding: 20,
+        justifyContent: 'center'
+    },
+    transactionContainer: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    titleBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    amount: {
+        marginVertical: 6,
+        fontWeight: 'bold',
+        color: '#da0',
+        fontSize: 20
     }
 })
 
